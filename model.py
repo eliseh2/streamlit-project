625d85bdbedfe03b6d1e33f678eca7e04aed1182
eliseh2 <167094630+eliseh2@users.noreply.github.com>
2024-07-25 10:43:52 +0800

    Update model.py

--- model.py
+++ model.py
@@ -25,6 +25,8 @@ ownerArr = ['2ND' , '1ST' , '3RD' , '4TH']
 fuelArr = ['DIESEL' , 'PETROL' , 'CNG']
 transmissionArr = ['Manual' , 'Automatic']
 body_typeArr = ['luxury%20suv' , 'hatchback' , 'sedan' , 'suv' , 'luxury%20sedan']
+companyArr = ['Mahindra' , 'Jeep' , 'Honda' , 'Toyota' , 'Hyundai' , 'Ford' , 'Maruti' , 'Renault',
+ 'Nissan', 'Volkswagen', 'Datsun', 'Tata', 'Skoda', 'KIA', 'MG',]
 
 
 # Create form inputs
@@ -33,6 +35,7 @@ owner = ownerArr.index(st.selectbox('Owner', ownerArr)) + 1
 fuel = fuelArr.index(st.selectbox('Fuel', fuelArr)) + 1
 transmission = transmissionArr.index(st.selectbox('Transmission', transmissionArr)) + 1
 body_type = body_typeArr.index(st.selectbox('Body Type', body_typeArr)) + 1
+company = companyArr.index(st.selectbox('Company' , companyArr)) + 1
 kms_driven = st.number_input('Kms Driven', min_value=0)
 
 airbags = st.selectbox('Airbags', [True, False])
@@ -40,10 +43,12 @@ alloy_wheels = st.selectbox('Alloy Wheels', [True, False])
 cruise_control = st.selectbox('Cruise Control', [True, False])
 steering_mounted_controls = st.selectbox('Steering Mounted Controls', [True, False])
 sunroof_moonroof = st.selectbox('Sunroof/Moonroof', [True, False])
+year = st.number_input('Year', min_value = 1 , max_value = 15)
+company = st.
 
 # Prepare input features
 features = [color, owner, fuel, transmission, body_type, kms_driven,
-            airbags, alloy_wheels, cruise_control, steering_mounted_controls, sunroof_moonroof]
+            airbags, alloy_wheels, cruise_control, steering_mounted_controls, sunroof_moonroof, year]
 
 # Make prediction
 if st.button('Predict Price'):
